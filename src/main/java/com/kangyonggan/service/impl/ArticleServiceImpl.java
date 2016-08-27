909f46ebec1657139352495b0bd57c0f5bc92691
kangyonggan <kangyonggan@email.com>
2016-08-27 20:39:19 +0800

    修复查询文件条件的bug

--- src/main/java/com/kangyonggan/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/kangyonggan/service/impl/ArticleServiceImpl.java
@@ -61,7 +61,7 @@ public class ArticleServiceImpl extends BaseService<Article> implements ArticleS
         }
         if (StringUtil.isNotEmpty(endTime)) {
             Date end = DateUtil.next(endTime);
-            criteria.andLessThanOrEqualTo("createdTime", end);
+            criteria.andLessThanOrEqualTo("endTime", end);
         }
 
         example.setOrderByClause("id desc");
