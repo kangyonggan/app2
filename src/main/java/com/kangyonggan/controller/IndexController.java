7e745f99aeaaef3fefc3c472812a8c57a6543387
kangyonggan <kangyonggan@email.com>
2016-09-09 14:20:29 +0800

    test

--- src/main/java/com/kangyonggan/controller/IndexController.java
+++ src/main/java/com/kangyonggan/controller/IndexController.java
@@ -1,8 +1,12 @@
 package com.kangyonggan.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 
 /**
  * 网站首页
@@ -12,10 +16,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @RequestMapping
+@Log4j2
 public class IndexController {
 
     private static final String PATH_ROOT = "web/index/";
     private static final String PATH_INDEX = PATH_ROOT + "index";
+
+    @RequestMapping("test")
+    public void test(HttpServletRequest request) {
+        log.info("交行一键支付-进入回调servlet");
+        try {
+            request.setCharacterEncoding("GBK");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            log.error("交行一键支付-签约回调异常：", e);
+            return;
+        }
+
+        String  notifyMsg = request.getParameter("notifyMsg"); //获取银行通知结果
+        log.info(notifyMsg);
+    }
 
     /**
      * 网站首页
